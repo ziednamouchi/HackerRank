28d22e01587acdcdfebf349aa98b3a796d3c76a5
Zied Namouchi <zyed.namouchy@gmail.com>
2017-02-06 21:40:39 +0100

    Update what_s_your_name.py

--- python/String/what_s_your_name.py
+++ python/String/what_s_your_name.py
@@ -1,2 +1,9 @@
 def print_full_name(a, b):
     print("Hello {} {}! You just delved into python.".format(a,b))
+'''other solution'''
+"""def print_full_name(a, b):
+    print("Hello %s %s! You just delved into python." % (a,b))"""
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
