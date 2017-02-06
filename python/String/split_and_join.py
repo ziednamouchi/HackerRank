b79222f58afffbcae6190d0c3fca153e5dae03f5
Zied Namouchi <zyed.namouchy@gmail.com>
2017-02-06 21:28:55 +0100

    Create split_and_join.py

---
+++ python/String/split_and_join.py
@@ -0,0 +1,9 @@
+def split_and_join(line):
+    splited = line.split(" ")
+    joined = "-".join(splited)
+    return joined
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
