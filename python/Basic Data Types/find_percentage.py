ae1b6f1334d38492c66d2a93d9e8d43a862f2b6a
Zied Namouchi <zyed.namouchy@gmail.com>
2017-02-15 21:34:34 +0100

    Update find_percentage.py

--- python/Basic Data Types/find_percentage.py
+++ python/Basic Data Types/find_percentage.py
@@ -5,10 +5,6 @@ if __name__ == '__main__':
         name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
-
     query_name = input()
-    percentage, s = 0, 0
-    for i in range(len(student_marks[query_name])):
-        s = s + student_marks[query_name][i]
-    percentage = s / len(student_marks[query_name])
+    percentage = sum(student_marks[query_name]) / len(student_marks[query_name])
     print("%.2f" % percentage)
